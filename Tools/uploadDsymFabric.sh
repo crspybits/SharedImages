7bff461ef801feaf4c821f346003f7ca84b4e585
Christopher Prince <chris@SpasticMuffin.biz>
2019-01-28 20:07:24 -0700

    Added comment to script.

--- Tools/uploadDsymFabric.sh
+++ Tools/uploadDsymFabric.sh
@@ -9,4 +9,7 @@
 # I pulled this from the Info.plist
 API_KEY="237a6efa41c3712c4dc41003288df88ae5178ace"
 
+# Note: I got warning: skipping Apple dSYM: /var/folders/50/3pr8rsks0zqfs1j53_k0l0nh0000gp/T///Users/chris/Desktop/dSYMs.zip.unzipped/C018F2DF-17EF-3DC8-AC79-82DED0DD6536.dSYM
+# seemingly when I tried to download the dSYM's from Apple iTunes Connect too early. E.g., just after processing completed. I got an archive with an odd extension, e.g., .dsym
+
 /Applications/Fabric.app/Contents/MacOS/upload-symbols -a $API_KEY -p ios $1
